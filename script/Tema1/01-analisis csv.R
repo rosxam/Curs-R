cd789ae57f744b7f39aacf75f867f25e6dde3caf
rosxam <36517153+rosxam@users.noreply.github.com>
2018-02-20 20:09:58 +0100

    20/02/2018 canvis team1

--- script/Tema1/01-analisis csv.R
+++ script/Tema1/01-analisis csv.R
@@ -1,2 +1,17 @@
 auto <- read.csv("../data/tema1/auto-mpg.csv",header = T, sep = ",")
 names(auto) 
+
+auto_no_header <- read.csv("../data/tema1/auto-mpg-noheader.csv", header = FALSE)
+head(auto_no_header, 4)
+tail(auto_no_header)
+
+auto_custom_header <- 
+  read.csv("../data/tema1/auto-mpg-noheader.csv",
+           header = FALSE,
+           col.names = c( "numeros","millas_por_galeon","cilindros",
+                          "desplazamiento","caballos_de_potencia","peso",
+                          "aceleración","año","modelo"
+                          )
+           )
+
+head(auto_custom_header,2)
